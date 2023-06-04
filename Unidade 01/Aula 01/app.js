3077a4f81a89e8417b4c02910dfd073a12869234
Gustavonn07 <gustavonepnog@gmail.com>
2023-06-03 23:34:09 -0300

    finalizei a 1 unidade (codigos)

---
+++ Unidade 01/Aula 01/app.js
@@ -0,0 +1,6 @@
+console.log('Hello World')
+
+// Na primeira aula ainda estamos preparando terreno e baixando os arquivos necessários
+// Baixar NodeJS e o VScode
+
+// A segunda aula é a mesma coisa, porém para Linux
