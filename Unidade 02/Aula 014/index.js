fafb1d4c6d16898da83b94b8076d1b15d6e0b4ae
Gustavonn07 <gustavonepnog@gmail.com>
2023-06-05 16:20:24 -0300

    finalizei a aula 14

---
+++ Unidade 02/Aula 014/index.js
@@ -0,0 +1,17 @@
+// Objeto Math
+
+// Arredondando:
+let num1 = 9.54578;
+let num2 = Math.floor(num1); // Para baixo
+let num3 = Math.ceil(num1); // Para cima
+let numrounded = Math.round(num1); // O mais perto
+console.log(num2);
+console.log(num3);
+console.log(numrounded);
+
+// Pegar o maior e menor valor de uma sequência:
+console.log(Math.max(1,2,3,4,5,6,67,8,10));
+console.log(Math.min(1,2,3,4,5,-6,67,8,10));
+
+// Pegar um número aleatorio:
+console.log(Math.random());
