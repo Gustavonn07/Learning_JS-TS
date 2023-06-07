182b1f2bf5078b3335b0621cfe934a27e3f3df2d
Gustavonn07 <gustavonepnog@gmail.com>
2023-06-07 16:27:18 -0300

    finalizei a aula 22

---
+++ Unidade 03/Aula 022/index.js
@@ -0,0 +1,45 @@
+// If e Else (Parte 01)
+
+// Usei função para facilitar, mas não precisa ta dentro.
+
+// Estruturam que ditam o que fazer.
+// if e else:
+function func(hora = 12){
+    if (hora <= 12){
+        console.log('Bom dia');
+    }
+    else{
+        console.log('Boa noite');
+    };
+
+}
+
+func(10); // Bom dia
+func(23); // Boa noite
+
+// Caso precise de mais condições pode usar if else (Quantas vezes quiser):
+
+function func2(hora = 100){
+    if (hora < 12){
+        console.log('Bom dia');
+    }
+    else if(hora >= 12 && hora <= 18){
+        console.log('Boa tarde');
+    }
+    else if(hora > 18 && hora <= 24){
+        console.log('Boa noite');
+    }
+    else{
+        console.log('Insira um valor certo');
+    };
+
+};
+
+func2(10); // Bom dia
+func2(15); // Boa tarde
+func2(23); // Boa noite
+func2(34); // Insira um valor correto
+func2();   // Insira um valor correto
+
+// If pode ser usado só.
+// Else e else if não podem ser usados sem if.
