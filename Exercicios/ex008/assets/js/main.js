7566e5ece52ad84c24d94eed5f315f0be501fa3a
Gustavonn07 <gustavonepnog@gmail.com>
2023-06-07 22:23:23 -0300

    atualizei a calc imc com operação ternária

--- Exercicios/ex008/assets/js/main.js
+++ Exercicios/ex008/assets/js/main.js
@@ -6,21 +6,24 @@ let altura = document.querySelector('#altura');
 function calcular(event) {
     event.preventDefault(); // Impede o comportamento padrão do formulário
 
-    let imc = peso.value / altura.value**2;
+    let imc = parseFloat(peso.value) / (parseFloat(altura.value) ** 2);
 
-    if (imc < 18.5) {
-        result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está Abaixo do peso`;
-    } else if (imc >= 18.5 && imc < 25) {
-        result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está no Peso normal`;
-    } else if (imc >= 25 && imc < 30) {
-        result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Sobrepeso`;
-    } else if (imc >= 30 && imc < 35) {
-        result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 1 grau`;
-    } else if (imc >= 35 && imc < 40) {
-        result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 2 grau`;
-    } else if (imc >= 40) {
-        result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 3 grau`;
-    }
+    // if (imc < 18.5) {
+    //     result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está Abaixo do peso`;
+    // } else if (imc >= 18.5 && imc < 25) {
+    //     result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está no Peso normal`;
+    // } else if (imc >= 25 && imc < 30) {
+    //     result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Sobrepeso`;
+    // } else if (imc >= 30 && imc < 35) {
+    //     result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 1 grau`;
+    // } else if (imc >= 35 && imc < 40) {
+    //     result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 2 grau`;
+    // } else if (imc >= 40) {
+    //     result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 3 grau`;
+    // };
+
+    // Testando operação ternária
+    const operacao = imc < 18.5 ? result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está Abaixo do peso`: imc >= 18.5 && imc < 25 ? result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está no Peso normal` : imc >= 25 && imc < 30 ? result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Sobrepeso` : imc >= 30 && imc < 35 ? result.innerHTML = `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 1 grau` : imc >= 35 && imc < 40 ? `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 2 grau` : `O seu IMC é de ${imc.toFixed(2)} e você está em Obesidade 3 grau`;
 }
 
 form.addEventListener('submit', calcular);
