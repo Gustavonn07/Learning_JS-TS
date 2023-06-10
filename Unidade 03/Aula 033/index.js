3b4e874622dde92e40f84167e38c97abbdab19aa
Gustavonn07 <gustavonepnog@gmail.com>
2023-06-10 09:18:42 -0300

    finalizei a aula 33

---
+++ Unidade 03/Aula 033/index.js
@@ -0,0 +1,11 @@
+// For of
+
+// Apenas para objetos interaveis (que possuem índice).
+
+// Não precisa entregar o índice:
+const pessoa = 'Gustavo';
+for (c of pessoa) {
+    console.log(c); // G U S T A V O
+}
+
+// Caso não use objeto interavel dá TypeERROR.
