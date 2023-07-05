1b06d15312f9da4e0fbef152b58a9a4afd410ab2
Gustavonn07 <gustavonepnog@gmail.com>
2023-07-05 10:35:15 -0300

    adicionando geradora

--- Unidade 04/Aula 048/index.js
+++ Unidade 04/Aula 048/index.js
@@ -26,3 +26,21 @@ console.log(g2.next().value);
 console.log(g2.next().value);
 console.log(g2.next().value);
 console.log(g2.next().value);
+
+// Podemos usar funções dentro de uma geradora dessa forma:
+
+function* geradora3() {
+    yield function () {
+        console.log('eae1');
+    }
+
+    yield function() {
+        console.log('eae2');
+    }
+};
+
+g3 = geradora3();
+f1 = g3.next().value;
+f2 = g3.next().value;
+f1();
+f2();
--- testes.js
+++ testes.js
@@ -1,1 +1,17 @@
 // Arquivo para salvar testes em javascript
+
+function* geradora() {
+    yield function () {
+        console.log('eae1');
+    }
+
+    yield function() {
+        console.log('eae2');
+    }
+}
+
+g1 = geradora();
+f1 = g1.next().value
+f2 = g1.next().value
+f1()
+f2()
