1d6019ebf778deef58e1a6f66dd381e3d5c46dd1
Gustavonn07 <gustavonepnog@gmail.com>
2023-07-11 19:09:03 -0300

    adicionei 3 metodos

--- Conteudos Extras/Metodos-de-string.js
+++ Conteudos Extras/Metodos-de-string.js
@@ -13,3 +13,22 @@ console.log(str1.indexOf('um'));
 console.log(str1.lastIndexOf('um'));
 console.log(str1.indexOf('um', 7));
     // Retorna 6 / 20 / 20
+
+// Extrair uma seção da string (substring não aceita valores negativos ---> viram 0): 
+console.log(str1.slice(0, 4));
+console.log(str1.substring(0, 4));
+console.log(str1.slice (10, -2));
+console.log(str1.substring(10, -2));
+    // Retorna Jão / Jão / pelido comum para o nome Joã / Jão é um a
+
+// Transformar uma string em seções (aceita limites):
+console.log(str1.split(' '));
+console.log(str1.split('o'));
+console.log(str1.split('o', 3));
+    // Separa a cada espaço / a cada letra o / a cada letra o com limite de 3
+
+// Achar um elemento em uma string (aceita posição para verificar):
+console.log(str1.includes('Jão'));
+console.log(str1.includes('apelido', 9));
+console.log(str1.includes('apelido', 10));
+    // retorna true / true / false
