dea861a88933f6bde32f082a04c10188bef2e672
Gustavonn07 <gustavonepnog@gmail.com>
2023-07-12 16:32:49 -0300

    adicionei 8  metodos

--- Conteudos Extras/Metodos-de-string.js
+++ Conteudos Extras/Metodos-de-string.js
@@ -2,6 +2,8 @@
 
 // Não será abordado propriedades de objetos string (lenght,...).
 const str1 = 'Jão é um apelido comum para o nome João.';
+const str2 = 'Essa é uma banana.';
+const str3 = '   Hello Mundo   ';
 
 // Acessar um caractere da string:
 console.log(str1[4]);
@@ -32,3 +34,36 @@ console.log(str1.includes('Jão'));
 console.log(str1.includes('apelido', 9));
 console.log(str1.includes('apelido', 10));
     // retorna true / true / false
+
+// Saber se o elemento começa com uma palavra: 
+console.log(str1.startsWith('Jão'));
+console.log(str1.startsWith('apelido'));
+    // retorna true / false
+
+
+// Saber se o elemento termina com uma palavra:
+console.log(str1.endsWith('.'));
+console.log(str1.endsWith('apelido'));
+    // retorna true / false
+
+// Para juntar strings use (Melhor usar += para performance):
+console.log(str1.concat(' ', str2));
+    // retorna Jão é um apelido comum para o nome João. Essa é uma banana
+
+// Para repetir strings use:
+console.log(str2.repeat(2));
+    // retorna Essa é uma banana.Essa é uma banana.
+
+// Para remover espaços em brancos do começo, fim e os dois use:
+console.log(str3.trimStart());
+console.log(str3.trimEnd());
+console.log(str3.trim());
+    // retorna Hello Mundo    /    Hello Mundo / Hello Mundo
+
+// Para adicionar espaços em branco no começo e no fim use (Pode preencher com outras str):
+
+console.log(str2.padStart(25));
+console.log(str2.padStart(25, 'eae'));
+console.log(str2.padEnd(25));
+console.log(str2.padEnd(25, 'eae'));
+    // retorna       Essa é uma banana. / eaeeaeeEssa é uma banana. / Essa é uma banana.        / Essa é uma banana.eaeeaee
