29916ce347ecc25432c05614767604977d0e9cb2
Gustavonn07 <gustavonepnog@gmail.com>
2023-09-08 11:08:21 -0300

    finalizei a aula 71

---
+++ Unidade 07/Aula071/index.js
@@ -0,0 +1,53 @@
+// Validando CPF com classe:
+
+class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: false,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        });
+    };
+
+    éSequencia() {
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2;
+    }
+
+    static geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for(let stringNumerica of cpfSemDigitos) {
+            total += reverso * Number(stringNumerica);
+            reverso--;
+        };
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
+    }
+
+    valida() {
+        if(!this.cpfLimpo) return false;
+        if(typeof this.cpfLimpo !== 'string') return false;
+        if(this.cpfLimpo.length !== 11) return false;
+        if(this.éSequencia()) return false;
+        this.geraNovoCpf()
+        
+        return this.novoCPF === this.cpfLimpo;
+    }
+};
+
+const validaCPF = new ValidaCPF('070.987.720-03');
+if(validaCPF.valida()) {
+    console.log('O CPF é válido!');
+} else {
+    console.log('O CPF não é válido!');
+}
